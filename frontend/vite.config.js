897f55f11456548098cae833a235ccb2b2311b46
AndreiHome <andrei.preoteasa.ap@gmail.com>
2024-01-25 19:46:51 +0200

    bux fixing actions

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     coverage: {
-      // you can include other reporters, but 'json-summary' is required, json is recommended
+      provider: 'v8',
       reporter: ['text', 'json-summary', 'json'],
       // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
       reportOnFailure: true,
