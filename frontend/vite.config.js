8ef4a83b1bccf6b29c025b539e8a8bbf5734d044
AndreiHome <andrei.preoteasa.ap@gmail.com>
2024-01-25 19:32:04 +0200

    updated actions testing workflow and package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    }
+  }
 })
