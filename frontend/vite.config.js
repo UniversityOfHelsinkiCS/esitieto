e22ff02432581202f8a2349810a0d1e709227f03
Owlaboy <andrei.preotesa.ap@gmail.com>
2024-01-31 15:28:05 +0200

    updated build and start scripts to work properly

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,6 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json-summary', 'json'],
-      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
       reportOnFailure: true,
     }
   }
