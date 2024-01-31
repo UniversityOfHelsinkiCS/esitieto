068fa522d7bb5d86d23ad6b1215830011c342c55
Owlaboy <andrei.preotesa.ap@gmail.com>
2024-01-31 13:56:43 +0200

    updated vite config file name

---
+++ frontend/vite.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3000, },
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    }
+  }
+})
