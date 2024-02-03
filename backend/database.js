99cb9b0af26a76eceb26da826200e0273c30e086
Jani <jankuul@gmail.com>
2024-02-03 14:38:20 +0200

    Some database work, it is now possible to fetch files from database through backend. Also included documentation to help with configuring

--- backend/database.js
+++ backend/database.js
@@ -1,4 +1,5 @@
-import { Client } from 'pg'
+//import { Client } from 'pg'
+const { Client } = require('pg');
 
 async function StartDatabase() {
     const client = new Client()
@@ -13,3 +14,18 @@ async function PrintCourses() {
 async function EndDatabase() {
     await client.end()
 }
+
+async function getCourses() {
+    console.log("test getCourses");
+    const client = new Client();
+    await client.connect();
+    
+    try {
+        const res = await client.query('SELECT * FROM courses');
+        return res.rows;
+    } finally {
+        await client.end();
+    }
+}
+
+module.exports = { getCourses };
