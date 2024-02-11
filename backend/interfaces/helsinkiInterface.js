68ef81166e7b0795bdf292057ab866059c2fed3c
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2024-02-11 12:25:34 +0200

    Added helsinkiInterface. Implements getlvNames and degreeStructure.

---
+++ backend/interfaces/helsinkiInterface.js
@@ -0,0 +1,75 @@
+
+class HelsinkiInterface {
+    /*
+    Interface to communicate with helsinki.fi APIs.
+
+    Methods:
+    degreeStructure - Gets degree structure using degree code
+    */
+    #requestOptions
+    
+    constructor() {
+        this.#requestOptions = {
+            method: 'GET',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            mode: 'cors',
+            cache: 'no-cache',
+        };
+    }
+  
+    isValidInput(input) {
+        /*Accepts numbers letters underscores and dashes*/
+        var pattern = /^[a-zA-Z0-9_\-]+( [a-zA-Z0-9_\-]+)*$/;
+  
+        return pattern.test(input);
+    }
+
+    async getlvNames() {
+        /*
+        Gets list of "lv_names" Academic year codes.
+
+        Returns the list of lv_names in JSON format.
+        */
+        const apiUrl = 'https://od.helsinki.fi/eduviewer/lv_names';
+        return fetch(apiUrl, this.#requestOptions)
+        .then(response => {
+        return response.json();
+
+        }).then(data => {
+        return data
+
+        }).catch(error => {
+        console.error('Fetch error:', error.message);
+        });
+
+    }
+  
+    async degreeStructure(id, lvName='hy-lv-74') {
+        /*
+        Gets the degree structure using <id> which should be the degree code.
+        Shows the <period> specific structure.
+
+        Returns the selected periods degree stucture in JSON format.
+        */
+        if (this.isValidInput(id) && this.isValidInput(lvName)) {
+            const apiUrl = 'https://od.helsinki.fi/eduviewer/tree_by_code/'.concat(encodeURIComponent(id)).concat('?lv=').concat(lvName);
+            return fetch(apiUrl, this.#requestOptions)
+            .then(response => {
+            return response.json();
+            }).then(data => {
+            return data
+
+            }).catch(error => {
+            console.error('Fetch error:', error.message);
+            });
+        } else {
+            return "Virheellinen syöte"
+        }
+    }
+
+
+}
+
+module.exports = HelsinkiInterface;
