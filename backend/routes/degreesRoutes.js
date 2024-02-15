51ef67859224c6e608200b468ea44ad3c04d2671
Jani <jankuul@gmail.com>
2024-02-15 20:19:15 +0200

    added ability to fetch degree data via backend using helsinki interface

--- backend/routes/degreesRoutes.js
+++ backend/routes/degreesRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const HelsinkiInterface = require('../interfaces/helsinkiInterface');
+const helsinkiInterface = new HelsinkiInterface();
 
 // Sample data. Will later include the courses that are part of the degree
 const degrees = [
@@ -10,5 +12,39 @@ const degrees = [
 router.get('/', (request, response) => {
   response.json(degrees);
 });
+
+router.get('/search_by_degree_name', async (req, res) => {
+  const id = req.query.search; // example KH50_005
+  console.log("Fetching degree with id:",id)
+  try {
+      const degreeStructure = await helsinkiInterface.degreeStructure(id);
+      const courseUnitRules = findAllCourseUnitRules(degreeStructure)
+
+      res.json(courseUnitRules);
+  } catch (error) {
+      console.error('Error fetching degree structure:', error.message);
+      res.status(500).send('Server error');
+  }
+});
+
+
+
+function findAllCourseUnitRules(obj, results = []) {
+  // Check if obj is an object or an array to iterate over its properties
+  if (obj !== null && typeof obj === 'object') {
+      // If the current object is the one we're looking for, add it to the results array
+      if (obj.type === 'CourseUnitRule') {
+          results.push(obj);
+      }
+
+      // Iterate over all properties of the object or elements of the array
+      Object.values(obj).forEach(value => {
+          // Recursively search for CourseUnitRule objects
+          findAllCourseUnitRules(value, results);
+      });
+  }
+
+  return results;
+}
 
 module.exports = router;
