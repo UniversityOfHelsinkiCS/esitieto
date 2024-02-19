3d742ad95af27f5346a46f997c8a31be4074449c
Owlaboy <andrei.preotesa.ap@gmail.com>
2024-02-19 13:41:34 +0200

    updated frontend vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,20 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    watch: {
+      usePolling: true,
+    },
     host: true,
     strictPort: true,
-    port: 3000, },
+    port: 3000,
+  },
   test: {
     coverage: {
       provider: 'v8',
