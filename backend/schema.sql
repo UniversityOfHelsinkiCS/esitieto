2e9c4b0ed6e0b53b78f55b946a1b95f80bc64880
Timo Paloneva <timo.paloneva@gmail.com>
2024-02-22 13:42:01 +0200

    updated schema to current state of app and added table for comments

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,37 +1,42 @@
-CREATE TYPE course_recommendation_type AS ENUM ('mandatory', 'optional', 'alt_mandatory');
-
-CREATE TABLE Courses (
+CREATE TABLE course_info (
     id SERIAL PRIMARY KEY,
-    course_code VARCHAR(255) NOT NULL,
+    kori_name VARCHAR(50) NOT NULL,
     course_name VARCHAR(255) NOT NULL,
-    course_nick_name VARCHAR(255),
-    kori_name VARCHAR(255) NOT NULL
+    official_course_id VARCHAR(50) NOT NULL,
+    CONSTRAINT course_kori_name_unique UNIQUE (kori_name)
 );
 
-CREATE TABLE Degree (
+CREATE TABLE degree_info (
     id SERIAL PRIMARY KEY,
+    kori_name VARCHAR(50) NOT NULL,
     degree_name VARCHAR(255) NOT NULL,
-    kori_name VARCHAR(255) NOT NULL
+    official_degree_id VARCHAR(50) NOT NULL,
+    CONSTRAINT degree_kori_name_unique UNIQUE (kori_name)
 );
 
-CREATE TABLE CourseDegreeRelation (
+CREATE TABLE course_degree_relation (
     id SERIAL PRIMARY KEY,
-    course_id INT,
-    degree_id INT,
-    FOREIGN KEY (course_id) REFERENCES Courses(id),
-    FOREIGN KEY (degree_id) REFERENCES Degree(id),
-    UNIQUE (course_id, degree_id)
+    degree_kori_name VARCHAR(50) NOT NULL,
+    course_kori_name VARCHAR(50) NOT NULL,
+    CONSTRAINT no_duplicate_course_degree_relation UNIQUE (degree_kori_name, course_kori_name)
 );
 
-CREATE TABLE PrerequisiteCourse (
-    course_id INT,
-    prerequisite_course_id INT,
-    course_type course_recommendation_type,
-    PRIMARY KEY (course_id, prerequisite_course_id),
-    FOREIGN KEY (course_id) REFERENCES Courses(id),
-    FOREIGN KEY (prerequisite_course_id) REFERENCES Courses(id)
+CREATE TABLE prerequisite_course_relation (
+    id SERIAL PRIMARY KEY,
+    course_kori_name VARCHAR(50) NOT NULL,
+    prerequisite_course_kori_name VARCHAR(50) NOT NULL,
+    CONSTRAINT unique_course_prerequisite UNIQUE (course_kori_name, prerequisite_course_kori_name),
+    CONSTRAINT no_self_prerequisite CHECK (course_kori_name != prerequisite_course_kori_name)
 );
 
-CREATE INDEX idx_course_degree_relation_degree_id ON CourseDegreeRelation(degree_id);
-CREATE INDEX idx_prerequisite_course_course_id ON PrerequisiteCourse(course_id);
-CREATE INDEX idx_prerequisite_course_prerequisite_course_id ON PrerequisiteCourse(prerequisite_course_id);
+CREATE TABLE course_prerequisite_comments (
+    id SERIAL PRIMARY KEY,
+    prerequisite_course_relation_id INT NOT NULL,
+    comment VARCHAR(1000) NOT NULL,
+    FOREIGN KEY (prerequisite_course_relation_id) REFERENCES prerequisite_course_relation(id)
+);
+
+
+CREATE INDEX idx_prerequisite_course_course_kori_name ON prerequisite_course_relation(course_kori_name);
+CREATE INDEX idx_prerequisite_course_prerequisite_course_kori_name ON prerequisite_course_relation(prerequisite_course_kori_name);
+CREATE INDEX idx_course_degree_relation_id ON course_degree_relation(degree_kori_name);
