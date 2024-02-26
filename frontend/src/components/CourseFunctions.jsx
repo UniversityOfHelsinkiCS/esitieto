cc65669ba872e7d244fe7e60178759b9afa98121
Jani <jankuul@gmail.com>
2024-02-26 12:32:24 +0200

    Added buttons for dependencies in frontend

--- frontend/src/components/CourseFunctions.jsx
+++ frontend/src/components/CourseFunctions.jsx
@@ -1,3 +1,4 @@
+// Add a course to the database
 export const addCourse = async (axios, onCoursesUpdated) => {
     const course_name = prompt("Enter course name:");
     if (!course_name) return;
@@ -12,6 +13,7 @@ export const addCourse = async (axios, onCoursesUpdated) => {
     onCoursesUpdated("fetch");
 };
 
+// Remove a course from the database
 export const removeCourse = async (axios, onCoursesUpdated) => {
     const kori_name = prompt("Enter Kori name of the course to remove:");
     if (!kori_name) return;
@@ -20,6 +22,7 @@ export const removeCourse = async (axios, onCoursesUpdated) => {
     onCoursesUpdated("fetch");
 };
 
+// Fetch a course and it's dependencies from database
 export const handleSearch = async (axios, onCoursesUpdated) => {
     const kori_name = prompt("Enter Kori name to search for its prerequisites:");
     if (!kori_name) return;
@@ -31,6 +34,42 @@ export const handleSearch = async (axios, onCoursesUpdated) => {
         console.error("Error fetching course with prerequisites: ", error);
     }
 };
+
+// Add a dependency to the database between two courses
+export const handleAddDependency = async (axios) => {
+    const course_kori_name = prompt("Enter the Kori name of the course:");
+    if (!course_kori_name) return;
+
+    const prerequisite_course_kori_name = prompt("Enter the Kori name of the prerequisite course:");
+    if (!prerequisite_course_kori_name) return;
+
+    try {
+        const response = await axios.post('/api/courses/addPrerequisiteCourse', { course_kori_name, prerequisite_course_kori_name });
+        console.log("Dependency added successfully:", response.data);
+    } catch (error) {
+        console.error("Error adding course dependency: ", error);
+    }
+};
+
+// Remove a dependency from the database between two courses
+export const handleRemoveDependency = async (axios) => {
+    const course_kori_name = prompt("Enter the Kori name of the course:");
+    if (!course_kori_name) return;
+
+    const prerequisite_course_kori_name = prompt("Enter the Kori name of the prerequisite course to remove:");
+    if (!prerequisite_course_kori_name) return;
+
+    try {
+        await axios.delete('/api/courses/removePrerequisiteCourse', {
+            data: { course_kori_name, prerequisite_course_kori_name }
+        });
+        console.log("Dependency removed successfully");
+    } catch (error) {
+        console.error("Error removing course dependency: ", error);
+    }
+};
+
+
 
 export const handleKORIAPITEST = async (axios) => {
     const searchTerm = prompt("Enter search term:");
