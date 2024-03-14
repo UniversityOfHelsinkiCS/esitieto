007a4321c1df564e2574e69b17479084d6323c98
lassisav <lassi.savolainen@helsinki.fi>
2024-03-14 13:18:56 +0200

    Added note of app being a course project and a link to GitHub to info box.

--- frontend/src/components/InfoBox.jsx
+++ frontend/src/components/InfoBox.jsx
@@ -8,6 +8,9 @@ export function InfoBox({ isOpen, onClose }) {
             <h2>Course prerequisite visualization tool</h2>
             <p>This application shows the necessary course prerequsities for certain degree programs in the University of Helsinki.</p>
             <p>Currently available is a sample version of the Computer Science bachelor's degree program.</p>
+            <p> </p>
+            <p>This application is being created as a project for the course Ohjelmistotuotanto for the University of Helsinki</p>
+            <p>Source code can be found <a href="https://github.com/Kurssiesitieto/kurssiesitieto-ohtuprojekti">here</a> </p>
             <button onClick={onClose}>Close</button>
         </div>
     )
