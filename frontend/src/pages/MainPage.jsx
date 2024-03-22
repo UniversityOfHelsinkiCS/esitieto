22aa972401e033dd7dd4314bf576d3ee4410928c
Jani <jankuul@gmail.com>
2024-03-22 11:24:56 +0200

    Refactored some code from frontend and commented some unnecessary parts off for potential cleanup later

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -4,48 +4,53 @@ import Sidebar from '../components/sidebar';
 import Course from '../models/Course'
 import DegreeSelectionMenu from '../components/DegreeSelectionMenu';
 import Messenger from '../components/messager/MessagerComponent';
-import { info, error } from '../components/messager/messager';
+import { info, error as displayError } from '../components/messager/messager';
+//import { list } from 'postcss'; Unused by eslint, unsure if someone using but nuke otherwise.
 
 const MainPage = ({ axiosInstance }) => {
+  const [degree, setDegree] = useState('TKT 23-26');
+  const [listOfDegrees, setDegreeToList] = useState([]);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [selectedCourseName, setSelectedCourseName] = useState('');
   const [courses, setCourses] = useState([]);
 
-  const fetchCourses = async (degree = null) => {
+  const fetchCourses = async (degree) => {
     try {
-      console.log("Fetching courses using degree", degree)
-      let response;
-      if (degree == null) {
-        response = await axiosInstance.get(`/api/courses`);
-        if (response == null) return;
-        setCoursesData(response.data);
+      if(degree == null) {
+        displayError("Jokin meni pieleen tutkintotietoja haettaessa!")
+        console.error("Degree is null!")
         return;
       }
 
-      console.log("fetching courses from degree", degree);
+      let response;
+      console.log("Fetching courses from degree", degree);
 
       response = await axiosInstance.get(`/api/degrees/search_by_degree`, {
-        headers: {
+        headers: { // HARDCODED INFO, NEEDS FIXING LATER.
           'degree-name': "kh50_005", //degree-name,
           'degree-years': '2023-2026', //degree-year
         }
       });
 
-      console.log("Response", response);
-
-      setCoursesData(response.data);
-
       if (response == null) {
-        error("Failed not fetch the degree!");
+        displayError("Jokin meni pieleen kurssitietoja haettaessa!");
+        console.error("Response is null!")
         return;
       } 
+
+      const convertedCourses = response.data.map(courseData => new Course(courseData.name, courseData.identifier, courseData.groupId, courseData.dependencies, courseData.type, courseData.description));
+      setCourses(convertedCourses);
       info("Fetched degree successfully");
 
     } catch (error) {
       console.error("Error fetching data: ", error);
-      error("Failed to fetch the data!");
+      displayError("Jokin meni pieleen!");
     }
   };
 
-  const setCoursesData = (data = null, fromDatabase = null) => {
+  const setCoursesData = (data = null, fromDatabase = null) => { // Likely to be nuked soon! But leaving here just in case.
+    return;
+    /*
     if (data == null) {
       console.log("No data to set courses!");
       return;
@@ -80,51 +85,39 @@ const MainPage = ({ axiosInstance }) => {
     // The description will be fetched from kori API when the sidebar is opened due to retrieving the most up-to-date scheduling, so this implementation will probably change.
     const convertedCourses = data.map(courseData => new Course(courseData.name, courseData.identifier, courseData.groupId, courseData.dependencies, courseData.type, courseData.description));
     setCourses(convertedCourses);
+    */
   };
 
   const fetchDegrees = async () => {
     try {
       const response = await axiosInstance.get(`/api/degrees`);
-      if (response == null) return;
-      setDegreeData(response.data);
+      if (response == null) {
+        displayError("Palvelimelle ei saatu yhteyttä")
+        return;
+      }
+      const convertedDegrees = response.data.map(degreeData => degreeData.degree_name);
+      setDegreeToList(convertedDegrees);
     } catch (error) {
-      console.error("Error fetching degree data: ", error);
+      console.error("Error when fetching degree data: ", error);
+      displayError("Jokin meni pieleen. Yritä uudestaan myöhemmin.")
     }
   };
-
-  const setDegreeData = (data = null) => {
-    if (data == null) {
-      console.log("No data to set degrees!");
-      return;
-    } else if (data === "fetch") {
-      fetchDegrees();
-      return;
-    }
-
-    const convertedDegrees = data.map(degreeData => degreeData.degree_name);
-    setDegreeToList(convertedDegrees);
-  };
-
-  useEffect(() => {
-    fetchCourses();
-  }, []);
 
   useEffect(() => {
     fetchDegrees();
   }, []);
+  
+  useEffect(() => {
+    if (listOfDegrees == null || listOfDegrees.length === 0) return
+    fetchCourses(listOfDegrees[0]);
+    
+  }, [listOfDegrees]);
 
   const handleDegreeChange = (degree) => {
     console.log("Selected Degree: ", degree);
     setDegree(degree);
     fetchCourses(degree)
   };
-
-
-  // What is the default degree? This needs to be solved later
-  const [degree, setDegree] = useState('TKT 23-26');
-  const [listOfDegrees, setDegreeToList] = useState([]);
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [selectedCourseName, setSelectedCourseName] = useState('');
 
   return (
     <div>
