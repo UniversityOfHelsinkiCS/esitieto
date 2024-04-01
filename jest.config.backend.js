a58b98df59aa75119855b7597487b066e36a4b5a
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2024-04-01 11:06:18 +0300

    Updated ignored folders for backend coverage.

--- jest.config.backend.js
+++ jest.config.backend.js
@@ -92,7 +92,7 @@ const config = {
   moduleNameMapper: {"\\.(css|less|scss|sass)$": "identity-obj-proxy"},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  modulePathIgnorePatterns: ["kurssiesitieto-ohtuprojekti/backend/archive/"],
+  modulePathIgnorePatterns: ["kurssiesitieto-ohtuprojekti/backend/archive/","kurssiesitieto-ohtuprojekti/backend/coverage/", "kurssiesitieto-ohtuprojekti/backend/dist/"],
 
   // Activates notifications for test results
   // notify: false,
