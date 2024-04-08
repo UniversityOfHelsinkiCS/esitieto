891690c722a318a0314009da5b0f53a650d9529e
Jani <jankuul@gmail.com>
2024-04-08 15:22:03 +0300

    Midcommit, still fixing add course test

--- backend/db/index.test.js
+++ backend/db/index.test.js
@@ -7,47 +7,69 @@ jest.mock('pg', () => {
   };
 });
 
+jest.mock('../interfaces/koriInterface', () => {
+  return jest.fn().mockImplementation(() => ({
+    searchCourses: jest.fn()
+  }));
+});
+
 
 const db = require('./index');
+const KoriInterface = require('../interfaces/koriInterface');
+
 
 describe('Database operations', () => {
   beforeEach(() => {
     jest.clearAllMocks();
+
+    KoriInterface.prototype.searchCourses = jest.fn().mockResolvedValue({
+      searchResults: [{ name: 'Intro to CS', groupId: 'CS101', code: 'IntroCS101' }]
+    });
   });
 
   // --- Courses ---
-
+  
   describe('addCourse', () => {
     it('should insert a course into the database and return the inserted course', async () => {
-      const mockCourse = { official_course_id: 'CS101', course_name: 'Intro to CS', kori_name: 'IntroCS101' };
+      const mockCourse = {
+        official_course_id: 'CS101', 
+        course_name: 'Intro to CS', 
+        kori_name: 'IntroCS101'
+      };
+      
       require('pg').Pool().query.mockResolvedValueOnce({ rows: [mockCourse], rowCount: 1 });
 
-      const result = await db.addCourse(mockCourse.official_course_id, mockCourse.course_name, mockCourse.kori_name);
+      const addedCourseCode = 'Intro to CS';
+      const result = await db.addCourse(addedCourseCode);
 
       expect(result).toEqual(mockCourse);
       expect(require('pg').Pool().query).toHaveBeenCalledWith(
-        'INSERT INTO course_info (official_course_id, course_name, kori_name) VALUES ($1, $2, $3) RETURNING *',
+        `INSERT INTO courses (kori_id, course_name, hy_course_id) SELECT $1, $2, $3 ON CONFLICT (kori_id) DO NOTHING RETURNING *`,
         [mockCourse.official_course_id, mockCourse.course_name, mockCourse.kori_name]
       );
     });
   });
+  
 
-  /*
-  describe('updateCourse', () => {
-    it('should update a course in the database and return the updated course', async () => {
-      const mockCourse = { id: 1, official_course_id: 'CS102', course_name: 'Advanced CS', kori_name: 'AdvCS102' };
-      require('pg').Pool().query.mockResolvedValueOnce({ rows: [mockCourse], rowCount: 1 });
+  describe('getCourses', () => {
+    it('should retrieve all courses from the database', async () => {
+      const mockCourses = [
+        { 
+          id: 1,
+          kori_id: 'CS101',
+          course_name: 'Intro to CS',
+          hy_course_id: 'IntroCS101'
+        }
+      ];
   
-      const result = await db.updateCourse(mockCourse.id, mockCourse.official_course_id, mockCourse.course_name, mockCourse.kori_name);
+      require('pg').Pool().query.mockResolvedValueOnce({ rows: mockCourses, rowCount: mockCourses.length });
   
-      expect(result).toEqual(mockCourse);
-      expect(require('pg').Pool().query).toHaveBeenCalledWith(
-        'UPDATE course_info SET official_course_id = $2, course_name = $3, kori_name = $4 WHERE id = $1 RETURNING *',
-        [mockCourse.id, mockCourse.official_course_id, mockCourse.course_name, mockCourse.kori_name]
-      );
+      const result = await db.getCourses();
+  
+      expect(result).toEqual(mockCourses);
+      expect(require('pg').Pool().query).toHaveBeenCalledTimes(1);
     });
   });
-  */
 
   describe('deleteCourse', () => {
     it('should delete a course from the database and return the rowCount', async () => {
@@ -61,25 +83,6 @@ describe('Database operations', () => {
         'DELETE FROM course_info WHERE kori_name = $1 RETURNING *',
         [kori_name]
       );
-    });
-  });
-
-  describe('getCourses', () => {
-    it('should retrieve all courses from the database', async () => {
-      const mockCourses = [
-        { 
-          official_course_id: 'CS101',
-          course_name: 'Intro to CS', 
-          kori_name: 'IntroCS101'
-        }
-      ];
-
-      require('pg').Pool().query.mockResolvedValueOnce({ rows: mockCourses, rowCount: mockCourses.length });
-  
-      const result = await db.getCourses();
-  
-      expect(result).toEqual(mockCourses);
-      expect(require('pg').Pool().query).toHaveBeenCalledTimes(1);
     });
   });
   
