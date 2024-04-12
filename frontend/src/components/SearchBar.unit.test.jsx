211b7152262994e0c79ffb4234ba79838c73d36a
AlTu774 <alisa.t1@outlook.com>
2024-04-12 17:43:41 +0300

    tried to mock fetchDatabaseSearchSuggestions again, but test doesn't work

--- frontend/src/components/SearchBar.unit.test.jsx
+++ frontend/src/components/SearchBar.unit.test.jsx
@@ -4,38 +4,37 @@ import {render, fireEvent} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import SearchBar from './SearchBar';
 
+
 describe("SearchBar unit testing", () => {
-    jest.mock('axios')
-    // const mockFetchDatabaseSearchSuggestions = jest.fn();
-    const mockAxiosInstance = jest.fn()
-    const mockHandleSearch = jest.fn()
+  const mockCourses = [
+    {
+    "id": 1,
+    "kori_id": "hy-CU-117375413",
+    "course_name": "Raja-arvot",
+    "hy_course_id": "MAT11003"
+  },
+  {
+    "id": 2,
+    "kori_id": "hy-CU-117375394",
+    "course_name": "Lineaarialgebra ja matriisilaskenta I",
+    "hy_course_id": "MAT11002"
+  }];
+  const mockFetchDatabaseSearchSuggestions = jest.fn().mockResolvedValue(mockCourses)
+  const mockHandleSearch = jest.fn()
+  const mockHandleChange = jest.fn()
+
 
     test("New text in textfield calls the onChange function", async () => {
-         axios.get = jest.fn().mockResolvedValueOnce({
-            data:
-            [{
-            "id": 1,
-            "kori_id": "hy-CU-117375413",
-            "course_name": "Raja-arvot",
-            "hy_course_id": "MAT11003"
-          },
-          {
-            "id": 2,
-            "kori_id": "hy-CU-117375394",
-            "course_name": "Lineaarialgebra ja matriisilaskenta I",
-            "hy_course_id": "MAT11002"
-          }]
-        });
-        
-        jest.spyOn(React, 'useEffect').mockImplementationOnce(f => f());
-
-        const {getByLabelText} = render(<SearchBar axiosInstance={mockAxiosInstance} handleSearch={mockHandleSearch}/>);
-
-        //await mockFetchDatabaseSearchSuggestions(mockAxiosInstance);
+        const {getByLabelText} = render(
+        <SearchBar 
+        axiosInstance={{}} 
+        handleSearch={mockHandleSearch} 
+        fetchDatabaseSearchSuggestions={mockFetchDatabaseSearchSuggestions}
+        handleChange={mockHandleChange}/>);
 
         const textfield = getByLabelText("Search courses")
         fireEvent.change(textfield, {target:{value:'test text'}});
-        expect(handleChange).toHaveBeenCalledWith('test text'); // eslint-disable-line
+        expect(mockHandleChange).toHaveBeenCalledWith('test text'); // eslint-disable-line
                                                                                       //<------------- Remove this comment after fixing the error with this line. handleChange is not defined.
     })
 
