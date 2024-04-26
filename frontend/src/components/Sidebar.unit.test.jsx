b7c2531b1d8d5d45a091e30921a4191bf0fee222
Timo Paloneva <timo.paloneva@gmail.com>
2024-04-26 17:30:35 +0300

    remove import from test that linter didnt like

--- frontend/src/components/Sidebar.unit.test.jsx
+++ frontend/src/components/Sidebar.unit.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Sidebar from './sidebar';
 import * as CourseFunctions from '../functions/CourseFunctions';
