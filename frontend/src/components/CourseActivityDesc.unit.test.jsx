1b7c467b49e76ddc2fb67526eddaae8c4b3f74d2
Timo Paloneva <timo.paloneva@gmail.com>
2024-04-29 13:59:06 +0300

    nuke some console.logs and fixed 1 lint error

--- frontend/src/components/CourseActivityDesc.unit.test.jsx
+++ frontend/src/components/CourseActivityDesc.unit.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CourseActivityDesc, findActivityPeriodDesc } from "./CourseActivityDesc";
+import { CourseActivityDesc } from "./CourseActivityDesc";
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
--- frontend/src/functions/CourseFunctions.jsx
+++ frontend/src/functions/CourseFunctions.jsx
@@ -1,11 +1,8 @@
 import { error as displayError } from '../components/messager/messager';
 
 export const handleFetchKORIByName = async (axios, searchTerm) => {
-    console.log("KORI search by name using name: ", searchTerm);
-
     try {
         const response = await axios.get(`/api/kori/search_by_name?search=${encodeURIComponent(searchTerm)}`);
-        //console.log(JSON.stringify(response.data, null, 2)); // Uncomment if you need to see the data
         return response.data
     } catch (error) {
         console.error("Error fetching searched courses from KORI:", error);
@@ -14,11 +11,8 @@ export const handleFetchKORIByName = async (axios, searchTerm) => {
 };
 
 export const handleFetchKORICourseInfo = async (axios, searchTerm) => {
-    console.log("KORI get course info using groupId: ", searchTerm);
-
     try {
         const response = await axios.get(`/api/kori/get_info_by_id?search=${encodeURIComponent(searchTerm)}`);
-        // console.log(JSON.stringify(response.data, null, 2)); // Uncomment if you need to see the data
         return response.data
     } catch (error) {
         console.error("Error fetching searched courses: ", error);
