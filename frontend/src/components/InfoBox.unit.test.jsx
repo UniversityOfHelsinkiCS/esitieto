d75f275e0742eb12004bdb01409d2b0b4a01fdba
sampsaoinonen <sampsa.oinonen@gmail.com>
2024-05-27 20:38:44 +0300

    Add coverage on InfoBox.unit.test.jsx

--- frontend/src/components/InfoBox.unit.test.jsx
+++ frontend/src/components/InfoBox.unit.test.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom'; // Tuodaan MemoryRouter
 import { InfoBox } from './InfoBox';
 
 describe('InfoBox', () => {
-    test('does not render when isOpen is false', () => {
-        const onCloseMock = jest.fn();
-        const { queryByText } = render(<InfoBox isOpen={false} onClose={onCloseMock} />);
-        expect(queryByText('Kurssin esitietojen visualisointityökalu')).toBeNull();
-    });
+  test('does not render when isOpen is false', () => {
+    const onCloseMock = jest.fn();
+    const { queryByText } = render(
+      <MemoryRouter>
+        <InfoBox isOpen={false} onClose={onCloseMock} />
+      </MemoryRouter>
+    );
+    expect(queryByText('Kurssin esitietojen visualisointityökalu')).toBeNull();
+  });
+
+  test('renders when isOpen is true', () => {
+    const onCloseMock = jest.fn();
+    const { getByText } = render(
+      <MemoryRouter>        
+        <InfoBox isOpen={true} onClose={onCloseMock} />
+      </MemoryRouter>
+    );
+    expect(getByText('Sulje')).toBeInTheDocument();
+  });
+
+  test('calls onClose when close button is clicked', () => {
+    const onCloseMock = jest.fn();
+    const { getByText } = render(
+      <MemoryRouter>
+        <InfoBox isOpen={true} onClose={onCloseMock} />
+      </MemoryRouter>
+    );
+    fireEvent.click(getByText('Sulje'));
+    expect(onCloseMock).toHaveBeenCalledTimes(1);
+  });
 });
