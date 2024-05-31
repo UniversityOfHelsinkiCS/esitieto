585d235b79315b6b9b57acf35314b061bf2623e3
sampsaoinonen <sampsa.oinonen@gmail.com>
2024-05-31 11:05:06 +0300

    Add coverage on MessagerComponent.unit.test.jsx

--- frontend/src/components/InfoBox.unit.test.jsx
+++ frontend/src/components/InfoBox.unit.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { MemoryRouter } from 'react-router-dom'; // Tuodaan MemoryRouter
+import { MemoryRouter } from 'react-router-dom'; 
 import { InfoBox } from './InfoBox';
 
 describe('InfoBox', () => {
