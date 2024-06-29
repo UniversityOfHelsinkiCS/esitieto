dcb4af27deb02ebdeddfe375dd2ea562d72bd31b
phuvio <petteri.huvio@gmail.com>
2024-06-29 19:13:25 +0300

    fix revert

--- backend/db/__mocks__/index.js
+++ backend/db/__mocks__/index.js
@@ -25,20 +25,25 @@ const addCourse = () => {
   };
 };
 
-const getCourses = () => {
-  return [{
-    id: 1,
-    kori_id: "CS101",
-    course_name: "Intro to CS",
-    hy_course_id: "IntroCS101"
-  }];
-};
+const mockGetCourses = jest.fn(() => {
+  return Promise.resolve({
+    rows: [
+      {
+        "id": "1",
+        "kori_id": "CS101",
+        "course_name": "Intro to CS",
+        "hy_course_id": "IntroCS101"
+      }
+    ],
+    rowcount: 1
+  });
+});
 
-const deleteCourse = (kori_name) => {
-  if (kori_name === "IntroCS101") {
-    return 1
+const addPrerequisiteCourse = (course_hy_id, prerequisite_course_hy_id) => {
+  if (course_hy_id !== prerequisite_course_hy_id) {
+    return [course_hy_id, prerequisite_course_hy_id];
   } else {
-    return 0
+    return [];
   }
 };
 
@@ -134,13 +139,8 @@ const getDegrees = (degreeCode, degreeYears) => {
 };
 
 module.exports = {
-  getStarted: mockGetStarted,
-  addCourse,
-  getCourses,
-  deleteCourse,
-  addPrerequisiteCourse,
-  removePrerequisiteCourse,
-  getDegrees,
-  getDegreeId: mockGetDegreeId,
+  getStarted: mockGetStarted, addCourse, getCourses: mockGetCourses,
+  deleteCourse, addPrerequisiteCourse, removePrerequisiteCourse,
+  getDegrees, getDegreeId: mockGetDegreeId,
   getCourseWithReqursivePrerequisites: mockGetCourseWithReqursivePrerequisites
 };
--- backend/routes/coursesRoutes.test.js
+++ backend/routes/coursesRoutes.test.js
@@ -3,6 +3,7 @@ const { findCourseWithDependencies } = require("./coursesRoutes");
 const request = require("supertest");
 const express = require("express");
 const app = express();
+const logger = require('../middleware/logger');
 app.use(express.json());
 app.use("/", routes);
 
@@ -30,10 +31,12 @@ describe("Get Courses", () => {
     kori_name: "IntroCS101"
   }];
   it('should get courses from database', async () => {
-    const result = await request(app).get("/databaseGetCourses");
-    expect(result._body[0].kori_id).toBe(courses[0].official_course_id);
-    expect(result._body[0].course_name).toBe(courses[0].course_name);
-    expect(result._body[0].hy_course_id).toBe(courses[0].kori_name);
+    const response = await request(app).get("/databaseGetCourses");
+    const result = JSON.parse(response.text).rows[0]
+    logger.debug("Courses from /databaseGetCourses", result)
+    expect(result.kori_id).toBe(courses[0].official_course_id);
+    expect(result.course_name).toBe(courses[0].course_name);
+    expect(result.hy_course_id).toBe(courses[0].kori_name);
   });
 });
 
