2405f5f638b00ccaa87e92a2ba88f4fe7596d01f
phuvio <petteri.huvio@gmail.com>
2024-06-29 19:32:30 +0300

    fix revert

--- backend/db/__mocks__/index.js
+++ backend/db/__mocks__/index.js
@@ -61,40 +61,6 @@ const mockGetDegreeId = jest.fn(() => {
   });
 });
 
-const mockGetCourseWithReqursivePrerequisites = jest.fn(() => {
-  return Promise.resolve({
-    //prerequisities for MAT21001 Lineaarialgebra ja matriisilaskenta II
-    rows: [
-      {
-        "id": "10",
-        "kori_id": "hy-CU-117375394",
-        "course_name": "Lineaarialgebra ja matriisilaskenta I",
-        "identifier": "MAT11002",
-        "dependencies": [
-          "MAT11001"
-        ]
-      },
-      {
-        "id": "5",
-        "kori_id": "hy-CU-117375151",
-        "course_name": "Johdatus yliopistomatematiikkaan",
-        "hy_course_id": "MAT11001",
-        "dependencies": []
-      },
-      {
-        "id": "16",
-        "kori_id": "hy-CU-117375754",
-        "course_name": "Lineaarialgebra ja matriisilaskenta II",
-        "identifier": "MAT21001",
-        "dependencies": [
-          "MAT11002"
-        ]
-      }
-    ],
-    rowcount: 3
-  });
-});
-
 const addPrerequisiteCourse = (course_hy_id, prerequisite_course_hy_id) => {
   if (course_hy_id !== prerequisite_course_hy_id) {
     return [course_hy_id, prerequisite_course_hy_id];
