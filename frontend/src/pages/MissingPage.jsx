06901a8580e6235fd53fa41d4d386818d060ab4e
phuvio <petteri.huvio@gmail.com>
2024-07-19 16:16:40 +0300

    add missing page for other URL's than allowed

---
+++ frontend/src/pages/MissingPage.jsx
@@ -0,0 +1,11 @@
+const MissingPage = ({axiosInstance}) => {
+    
+    return (
+        <div>
+            Virhe 404.
+            Sivua ei löytynyt.
+        </div>
+    );
+}
+
+export default MissingPage;
