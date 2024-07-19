a6a016eded8091fbfae91a4b8f4c8d2d5b945586
sampsaoinonen <sampsa.oinonen@gmail.com>
2024-07-19 16:01:44 +0300

    Filter out invalid environment variables to fix build error on Windows

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,10 +9,24 @@ const inDeployment = process.env.DEPLOYMENT === 'true'
 const devPort = process.env.VITE_DEV_PORT ? parseInt(process.env.VITE_DEV_PORT, 10) : 3001;
 console.log('VITE_DEV_PORT:', devPort);
 
+// List of environment variables to exclude
+const excludeEnvVars = [
+  'ProgramFiles(x86)',
+  'CommonProgramFiles(x86)',
+];
+
+// Filter the environment variables to exclude the problematic ones
+const filteredEnv = Object.keys(process.env).reduce((env, key) => {
+  if (!excludeEnvVars.includes(key)) {
+    env[key] = process.env[key];
+  }
+  return env;
+}, {});
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), EnvironmentPlugin('all')],
+  plugins: [react(), EnvironmentPlugin(filteredEnv)],
   base: inDeployment ? '/esitieto' : '/',
   server: {
     proxy: {
