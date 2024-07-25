7fc67c8e0d19642c0dd6795fa1da52ab50eac87a
phuvio <petteri.huvio@gmail.com>
2024-07-25 18:06:04 +0300

    refactor user middleware

--- backend/middleware/user.js
+++ backend/middleware/user.js
@@ -1,7 +1,7 @@
 const parseIamGroups = (iamGroups) =>
-  iamGroups?.split(';').filter(Boolean) ?? []
+  iamGroups?.split(';').filter(Boolean) ?? [];
 
-const checkAdmin = (iamGroups) => iamGroups.includes('grp-toska')
+const checkAdmin = (iamGroups) => iamGroups.includes('grp-toska');
 
 const mockHeaders = {
     uid: 'testUser',
@@ -9,52 +9,58 @@ const mockHeaders = {
     preferredlanguage: 'fi',
     hypersonsisuid: 'hy-hlo-123456789',
     hygroupcn: 'grp-toska;hy-employees',
-}
-
-const userMiddleware = async (req, _res, next) => {
-    const headers = process.env.NODE_ENV === 'production' ? req.headers : mockHeaders
-
-    if (headers.uid == null) {
-        req.kirjauduttu = false;
-
-        return next()
-    }
-    const validateHelsinkiEmail = (email) => {
-        // Define the regular expression pattern to match emails ending with @helsinki.fi
-        const helsinkiEmailPattern = /^[a-zA-Z0-9._%+-]+@helsinki\.fi$/;
-      
-        return helsinkiEmailPattern.test(email);
-    };
-    if (!validateHelsinkiEmail(headers.mail)) {
-        req.kirjauduttu = false;
-
-        return next()
-    }
-    req.kirjauduttu = true;
-
-    const {
-        uid: username,
-        mail: email,
-        preferredlanguage: language,
-        hypersonsisuid: id,
-        hygroupcn,
-    } = headers
-
-    const iamGroups = parseIamGroups(hygroupcn)
-
-    const user = {
-        id,
-        username,
-        email,
-        language,
-        iamGroups,
-        isAdmin: checkAdmin(iamGroups),
-    }
-
-    req.user = user
-
-    return next()
 };
+
+const validateHelsinkiEmail = (email) => {
+    // Define the regular expression pattern to match emails ending with @helsinki.fi
+    const helsinkiEmailPattern = /^[a-zA-Z0-9._%+-]+@helsinki\.fi$/;
+  
+    return helsinkiEmailPattern.test(email);
+};
+
+const getHeaders = (req) => process.env.NODE_ENV === 'production' ? req.headers : mockHeaders;
+
+const areHeadersValid = (headers) => {
+    if (!headers.uid || !validateHelsinkiEmail(headers.mail)) {
+      return false;
+    }
+    return true;
+};
+
+const createUser = (headers) => {
+    const {
+      uid: username,
+      mail: email,
+      preferredlanguage: language,
+      hypersonsisuid: id,
+      hygroupcn,
+    } = headers;
+  
+    const iamGroups = parseIamGroups(hygroupcn);
+  
+    return {
+      id,
+      username,
+      email,
+      language,
+      iamGroups,
+      isAdmin: checkAdmin(iamGroups),
+    };
+};
+
+const userMiddleware = (req, _res, next) => {
+    const headers = getHeaders(req);
+  
+    if (!areHeadersValid(headers)) {
+      req.kirjauduttu = false;
+      return next();
+    }
+  
+    req.kirjauduttu = true;
+    req.user = createUser(headers);
+  
+    return next();
+  };
 
 module.exports = {
     userMiddleware,
