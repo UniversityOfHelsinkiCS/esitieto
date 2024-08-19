c90f05d87c96cb7d6e44db1afaaf07a1374e1282
Petteri Huvio <petteri.huvio@gmail.com>
2024-08-19 14:29:25 +0300

    test Shibboleth, change console log to show ckookies

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    console.log('loyda tama', req.headers)
-    console.log('tassa toinen',req.body)
+    console.log('loyda tama', req.headers.cookie)
     res.json(req.headers);
     console.log(res.json)
 });
