ba068ffc11eec72139324c8b5ee9eda27f7915d6
Petteri Huvio <petteri.huvio@gmail.com>
2024-08-19 13:36:34 +0300

    test Shibboleth headers, change loginRoutes give req.headers

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    console.log(req.headers)
-    console.log(req.body)
-    console.log(res.headers)
-    console.log(res.body)
-    res.json(req.kirjauduttu);
+    console.log('loyda tama', req.headers)
+    console.log('tassa toinen',req.body)
+    console.log('kolkki', res.headers)
+    console.log('vimonen', res.body)
+    res.json(req.headers);
 });
 
 module.exports = router;
