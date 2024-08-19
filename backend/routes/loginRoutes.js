6bd53fde1a03480cd7a7b8aeea5d3b79c00f5caa
Petteri Huvio <petteri.huvio@gmail.com>
2024-08-19 14:52:56 +0300

    test Shibboleth, add console.log headers.uid

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     console.log('loyda tama', req.headers.cookie)
+    console.log('uid', req.headers.uid)
     res.json(req.headers);
     console.log(res.json)
 });
