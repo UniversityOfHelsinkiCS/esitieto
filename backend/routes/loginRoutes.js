25c249969bed201419d529342dc13da9443a77a3
Petteri Huvio <petteri.huvio@gmail.com>
2024-08-19 14:15:53 +0300

    test Shibboleth headers, add console login loginRoutes

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -4,9 +4,8 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     console.log('loyda tama', req.headers)
     console.log('tassa toinen',req.body)
-    console.log('kolkki', res.headers)
-    console.log('vimonen', res.body)
     res.json(req.headers);
+    console.log(res)
 });
 
 module.exports = router;
