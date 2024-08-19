cba121a797f7f51ece0e01a141422ccf39d86b53
Petteri Huvio <petteri.huvio@gmail.com>
2024-08-19 12:54:33 +0300

    test Shibboleth headers, change route to post

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    console.log(req.json)
-    console.log(res)
+    console.log(req.headers)
+    console.log(req.body)
+    console.log(res.headers)
+    console.log(res.body)
     res.json(req.kirjauduttu);
 });
 
